50da366c5c97c53f3000ef90400dfec0bc9131a9
Lukas <lukas@leucali.net>
2022-04-05 18:20:51 +0200

    Removed Code Smells

--- src/service/mockup/articleServiceMock.ts
+++ src/service/mockup/articleServiceMock.ts
@@ -102,15 +102,11 @@ export class ArticleServiceMock implements ArticleService {
 
   @Query((_returns) => [Article])
   getArticles(): Promise<Article[]> {
-    return new Promise((resolve, _reject) => {
-      resolve(this.articlesMock);
-    });
+    return Promise.resolve(this.articlesMock);
   }
 
   @Query((_returns) => Article)
   getArticle(@Arg("id") id: number): Promise<Article> {
-    return new Promise((resolve, _reject) => {
-      resolve(this.articlesMock.filter((articlesMock) => articlesMock.id === id)[0]);
-    });
+    return Promise.resolve(this.articlesMock.filter((articlesMock) => articlesMock.id === id)[0]);
   }
 }
