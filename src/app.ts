53be73ac43b7be9ec25cba0c7ca100c499528bef
Lukas <lukas@leucali.net>
2022-04-05 18:14:14 +0200

    Fixed Unit Tests

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ dataSource.initialize().catch((err) => console.error(err));
 /**
  * Express Configuration
  */
-export const api = express();
+const api = express();
 
 api.listen(process.env.API_PORT, () => {
   console.log(
