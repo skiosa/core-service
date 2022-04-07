55cd7bd217e4ce3636bc0ee00c5faf5a8ac9ad03
Tim Horlacher <tim.horlacher@protonmail.com>
2022-04-07 17:05:17 +0000

    Added keycloak

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "reflect-metadata";
-import { dataSource } from 'skiosa-orm/lib/db';
+import { dataSource } from "skiosa-orm/lib/db";
 import { defaultController } from "./controller/defaultController";
 import { errorController } from "./controller/errorController";
 import { graphqlController } from "./controller/graphqlController";
+import expressSession from "express-session";
+import KeycloakConnect, { KeycloakConfig } from "keycloak-connect";
+import session from "express-session";
 
 /**
  * Configuration Part
@@ -28,11 +31,49 @@ api.listen(process.env.API_PORT, () => {
 });
 
 /**
+ * Keycloak Configuration
+ */
+
+var memoryStore = new session.MemoryStore();
+var keycloak = new KeycloakConnect(
+  { store: memoryStore },
+  {
+    "auth-server-url": "https://keycloak.skiosa.de/auth",
+    realm: "Skiosa",
+    resource: "core-service",
+    "confidential-port": "443",
+    "ssl-required": "true",
+    "bearer-only": true,
+  }
+);
+
+api.use(
+  session({
+    secret: "mySecret",
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  })
+);
+
+api.use(keycloak.middleware());
+
+api.get(
+  "/protected",
+  keycloak.protect("realm:skiosa-user"),
+  function (req, res) {
+    console.log("User is authenticated!");
+    res.json("Hello User");
+  }
+);
+
+/**
  * Express Routes
  */
+
 api.use("/", defaultController);
 api.use("/graphql", graphqlController);
-api.use('*', errorController);
+api.use("*", errorController);
 
 /**
  * Security Configuration
