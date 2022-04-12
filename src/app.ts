775920bebeb013bec0e1f2f85625ae734ee613f9
Tim Horlacher <tim.horlacher@protonmail.com>
2022-04-12 20:41:55 +0200

    Skios 92 initialize keycloak (#7)
    * Added keycloak
    
    * Added middelware
    
    * Updated devcontainer
    
    * Added tests
    
    * Restored ts-target in tsconfig.json
    
    Co-authored-by: Lukas <lukas@leucali.net>

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import "reflect-metadata";
+import { dataSource } from "skiosa-orm/lib/db";
 import { defaultController } from "./controller/defaultController";
 import { errorController } from "./controller/errorController";
 import { graphqlController } from "./controller/graphqlController";
-import { dataSource } from "skiosa-orm/lib/db";
+import KeycloakConnect from "keycloak-connect";
+import session from "express-session";
+import { UserInfo } from "./model/jwt";
+
 
 /**
  * Configuration Part
@@ -23,17 +27,57 @@ dataSource
  */
 const api = express();
 
+declare module "express-serve-static-core" {
+  interface Request {
+    UserInfo?: UserInfo;
+  }
+}
+
 api.listen(process.env.API_PORT, () => {
   console.log(
     `Core-Service running at http://localhost:${process.env.API_PORT}`
   );
 });
+
+/**
+ * Session Store Configuration
+ */
+var memoryStore = new session.MemoryStore();
+api.use(
+  session({
+    secret: process.env.MEMORYSTORE_SECRET || "secret",
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  })
+);
+
+/**
+ * Keycloak Configuration
+ */
+var keycloak = new KeycloakConnect(
+  { store: memoryStore },
+  {
+    "auth-server-url": process.env.KEYCLOAK_URL || "http://localhost:5000/auth",
+    realm: process.env.KEYCLOAK_REALM || "skiosa-test",
+    resource: process.env.KEYCLOAK_RESOURCE || "skiosa-core-service",
+    "confidential-port": process.env.KEYCLOAK_PORT || "443",
+    "ssl-required": "true",
+    "bearer-only": true,
+  }
+);
+
+/**
+ * Express Middleware
+ */
+api.use(keycloak.middleware());
+
 /**
  * Express Routes
  */
 api.use("/", defaultController);
 api.use("/graphql", graphqlController);
-api.use('*', errorController);
+api.use("*", errorController);
 
 /**
  * Security Configuration
---
+++ src/model/jwt.ts
@@ -0,0 +1,7 @@
+export interface UserInfo {
+  id: string | undefined;
+  email: string | undefined;
+  username: string | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
+}
