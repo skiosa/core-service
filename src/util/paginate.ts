6c5c3e6c087babdc9a6340f496f8c3916e3111db
jeppard <35837126+jeppard@users.noreply.github.com>
2022-04-12 21:35:28 +0000

    Added comments

--- src/util/paginate.ts
+++ src/util/paginate.ts
@@ -1,4 +1,13 @@
 import { PaginationArg } from "../model/paginationArg";
+
+/**
+ * @author Jonas Eppard
+ * @summary Apply Pagination to a list
+ * @description Skip the first n elements and take the next n elements from the list
+ * @param {T[]} list - List to paginate
+ * @param {PaginationArg} paginated - Pagination Arguments skip and take
+ * @returns {T[]} List of paginated elements
+ */
 export function paginate<T>(arr: T[], pagination?: PaginationArg): T[] {
   return pagination ? arr.slice(pagination.skip ?? 0, (pagination.skip ?? 0) + pagination.take) : arr;
 }
