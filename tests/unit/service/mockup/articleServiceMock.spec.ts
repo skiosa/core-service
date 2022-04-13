b26854bce9c58733a361cf9ff70e3bd23e5e98c1
Theo Krinitz <the.krinitz@gmail.com>
2022-04-13 19:44:34 +0000

    fixed unittests

--- tests/unit/service/mockup/articleServiceMock.spec.ts
+++ tests/unit/service/mockup/articleServiceMock.spec.ts
@@ -5,15 +5,15 @@ describe("Mock-ArticleService", () => {
     const mockArticleService = new ArticleServiceMock();
 
     it("should return a list of articles", async () => {
-        let articles = await mockArticleService.getArticles();
+        let articles = await mockArticleService.articles();
 
         assert.isArray(articles);
         assert.equal(articles.length, 5);
     });
 
     it("should return a single article", async () => {
-        let article = await mockArticleService.getArticle(1);
-
+        let article = await mockArticleService.article(1);
+        
         assert.isObject(article);
         assert.equal(article.id, 1);
         assert.equal(article.title, "How installing linux made me gain 20lbs of muscle");
