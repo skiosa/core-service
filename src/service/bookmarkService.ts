1fd23bf602a2d909768b4c7484cc4f076727fde7
Lukas <lukas@leucali.net>
2022-05-03 12:16:34 +0200

    Fixed Comments

--- src/service/bookmarkService.ts
+++ src/service/bookmarkService.ts
@@ -7,6 +7,9 @@ export interface BookmarkService {
      * @author Lukas Huida
      * @summary add article to bookmarks
      * @description adds an article to the current user's bookmarks
+     * @param {number} articleId - id of an article to add to bookmarks
+     * @param {UserInfo} currentUserInfo - jwt info of logged in user
+     * @returns {boolean} status of bookmark for this article
      */
     addBookmark(currentUserInfo: UserInfo, articleId: number): Promise<boolean>;
 
@@ -14,6 +17,9 @@ export interface BookmarkService {
      * @author Lukas Huida
      * @summary delete article from bookmarks
      * @description removes an article from the current user's bookmarks
+     * @param {number} articleId - id of an article to add to bookmarks
+     * @param {UserInfo} currentUserInfo - jwt info of logged in user
+     * @returns {boolean} status of bookmark for this article
      */
     deleteBookmark(currentUserInfo: UserInfo, articleId: number): Promise<boolean>;
 
